84a8747b8f25d810950401a3ecf68296cc209dd3
Idowu Emehinola <hydeenoble39@gmail.com>
2021-11-13 21:07:25 +0100

    added web3 dependency

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,62 @@
 App = {
+    loading: false,
+    contracts: {},
 
+    load: async() => {
+        await App.loadWeb3()
+            // await App.loadAccount()
+            // await App.loadContract()
+            // await App.render()
+    },
+
+    // https://medium.com/metamask/https-medium-com-metamask-breaking-change-injecting-web3-7722797916a8
+    loadWeb3: async() => {
+        if (typeof web3 !== 'undefined') {
+            App.web3Provider = web3.currentProvider
+            web3 = new Web3(web3.currentProvider)
+        } else {
+            window.alert("Please connect to Metamask.")
+        }
+
+        if (window.ethereum) {
+            window.web3 = new Web3(ethereum);
+            try {
+                // Request account access if needed
+                await ethereum.enable();
+                // Acccounts now exposed
+                web3.eth.sendTransaction({ /* ... */ });
+            } catch (error) {
+                // User denied account access...
+            }
+        }
+        // Legacy dapp browsers...
+        else if (window.web3) {
+            window.web3 = new Web3(web3.currentProvider);
+            // Acccounts always exposed
+            web3.eth.sendTransaction({ /* ... */ });
+        }
+        // Non-dapp browsers...
+        else {
+            console.log('Non-Ethereum browser detected. You should consider trying MetaMask!');
+        }
+    },
+
+    loadContract: async() => {
+        // Create a JavaScript version of the smart contract
+        const robin = await $.getJSON('Robin.json')
+        App.contracts.Robin = TruffleContract(robin)
+        App.contracts.Robin.setProvider(App.web3Provider)
+
+        // Hydrate the smart contract with values from the blockchain
+        App.robin = await App.contracts.Robin.deployed()
+    },
+
+    loadAccount: async() => {
+        // Set the current blockchain account
+        App.account = web3.eth.accounts[0]
+
+        console.log(App.account)
+    },
 }
 
 $(() => {
